636dd01ff36dfd7844804de9cb755025431d6466
Tomas Votava <info@tomasvotava.eu>
2024-08-13 20:17:57 +0200

    feat!: utilize async lock to overcome dangerous race conditions
    Brought into light by @parikls in !186

--- examples/notion.py
+++ examples/notion.py
@@ -23,14 +23,14 @@ sso = NotionSSO(
 @get("/oauth2/login")
 async def auth_init():
     """Initialize auth and redirect"""
-    with sso:
+    async with sso:
         return await sso.get_login_redirect()
 
 
 @get("/oauth2/callback")
 async def auth_callback(request: Request):
     """Verify login"""
-    with sso:
+    async with sso:
         user = await sso.verify_and_process(request)
         return user
 
